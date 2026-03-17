602730e6554c717ba8879082ee9ff7d8efeb3078
Onkar-Satale <onkarsatale4@gmail.com>
2026-03-17 14:46:43 +0530

    Update API URL for production

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // prioritize production API URL if provided through Environment Variables, otherwise default to local
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  // prioritize production API URL if provided through Environment Variables, otherwise default to Render backend
+  baseURL: import.meta.env.VITE_API_URL || 'https://portfolio-2wyb.onrender.com/api',
 });
 
 export default apiClient;
